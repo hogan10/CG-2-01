1e3e4ca7b926173b3607a72914fdb107ceacc1ab
Erin Hogan <hogan10@charter.net>
2018-07-19 22:52:02 -0500

    Homework Complete

--- CG-2-01/Program.cs
+++ CG-2-01/Program.cs
@@ -6,7 +6,17 @@ namespace CG_2_01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+            Console.Write("Type your first name:");
+            string myFirstName;
+            myFirstName = Console.ReadLine();
+
+            string myLastName;
+            Console.Write("Type your last name:");
+            myLastName = Console.ReadLine();
+
+            Console.WriteLine("Hello," + myFirstName + " " + myLastName);
+            Console.ReadLine();
         }
     }
 }
